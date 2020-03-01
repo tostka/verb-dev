99808f28e47a27739dc17398a2481a318142c889
Tostka <tostka@users.noreply.github.com>
2020-03-01 17:22:57 -0600

    verb-dev
    moved new-githubgist from verb-desktop

---
+++ Public/New-GitHubGist.ps1
@@ -0,0 +1,123 @@
+﻿#*------v Function New-GitHubGist v------
+Function New-GitHubGist {
+    <#
+    .SYNOPSIS
+    New-GitHubGist.ps1 - Create GitHub Gist from passed param or file contents
+    .NOTES
+    Author: Jeffery Hicks
+    Website:	https://jdhitsolutions.com/blog/powershell/5410/creating-a-github-gist-with-powershell/
+    Twitter:	@tostka, http://twitter.com/tostka
+    Additional Credits: REFERENCE
+    Website:	URL
+    Twitter:	URL
+    REVISIONS   :
+    * 1/26/17 - posted version
+    .DESCRIPTION
+    .PARAMETER Name
+    What is the name for your gist?
+    PARAMETER Path
+    Path to file of content to be converted
+    PARAMETER Content,
+    Content to be converted
+    PARAMETER Description,
+    Description for new Gist
+    PARAMETER UserToken
+    Github Access Token
+    PARAMETER Private
+    Switch parameter that specifies creation of a Private Gist
+    PARAMETER Passthru
+    Passes the new Gist through into pipeline, as a new object
+    .EXAMPLE
+    New-GitHubGist -Name "BoxPrompt.ps1" -Description "a fancy PowerShell prompt function" -Path S:\boxprompt.ps1
+    .LINK
+    https://jdhitsolutions.com/blog/powershell/5410/creating-a-github-gist-with-powershell/
+    #>
+
+    [cmdletbinding(SupportsShouldProcess, DefaultParameterSetName = "Content")]
+    Param(
+        [Parameter(Position = 0, Mandatory, HelpMessage = "What is the name for your gist?", ValueFromPipelineByPropertyName)]
+        [ValidateNotNullorEmpty()]
+        [string]$Name,
+        [Parameter(ParameterSetName = "path", Mandatory, ValueFromPipelineByPropertyName)]
+        [ValidateNotNullorEmpty()]
+        [Alias("pspath")]
+        [string]$Path,
+        [Parameter(ParameterSetName = "Content", Mandatory)]
+        [ValidateNotNullorEmpty()]
+        [string[]]$Content,
+        [string]$Description,
+        [Alias("token")]
+        [ValidateNotNullorEmpty()]
+        [string]$UserToken = $gitToken,
+        [switch]$Private,
+        [switch]$Passthru
+    )
+
+    Begin {
+        Write-Verbose "[BEGIN  ] Starting: $($MyInvocation.Mycommand)"
+
+        #create the header
+        $head = @{
+            Authorization = 'Basic ' + $UserToken
+        }
+        #define API uri
+        $base = "https://api.github.com"
+
+    } #begin
+
+    Process {
+        #display PSBoundparameters formatted nicely for Verbose output
+        [string]$pb = ($PSBoundParameters | Format-Table -AutoSize | Out-String).TrimEnd()
+        Write-Verbose "[PROCESS] PSBoundparameters: `n$($pb.split("`n").Foreach({"$("`t"*2)$_"}) | Out-String) `n"
+
+        #json section names must be lowercase
+        #format content as a string
+
+        switch ($pscmdlet.ParameterSetName) {
+            "path" {
+                $gistContent = Get-Content -Path $Path | Out-String
+            }
+            "content" {
+                $gistContent = $Content | Out-String
+            }
+        } #close Switch
+
+        $data = @{
+            files       = @{$Name = @{content = $gistContent } }
+            description = $Description
+            public      = (-Not ($Private -as [boolean]))
+        } | Convertto-Json
+
+        Write-Verbose ($data | out-string)
+        Write-Verbose "[PROCESS] Posting to $base/gists"
+
+        If ($pscmdlet.ShouldProcess("$name [$description]")) {
+
+            #parameters to splat to Invoke-Restmethod
+            $invokeParams = @{
+                Method      = 'Post'
+                Uri         = "$base/gists"
+                Headers     = $head
+                Body        = $data
+                ContentType = 'application/json'
+            }
+
+            $r = Invoke-Restmethod @invokeParams
+
+            if ($Passthru) {
+                Write-Verbose "[PROCESS] Writing a result to the pipeline"
+                $r | Select @{Name = "Url"; Expression = { $_.html_url } },
+                Description, Public,
+                @{Name = "Created"; Expression = { $_.created_at -as [datetime] } }
+            }
+        } #should process
+
+    } #process
+
+    End {
+        Write-Verbose "[END    ] Ending: $($MyInvocation.Mycommand)"
+    } #end
+
+} #end function
+#*------^ END Function New-GitHubGist ^------
+# moved SendTo-Gist to ise-prof.ps1
