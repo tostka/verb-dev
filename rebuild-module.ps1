937090d9ebb11a6987c46ee7465ef065e9689af7
Tostka <tostka@users.noreply.github.com>
2020-04-01 15:17:54 -0500

    verb-dev
    * 9:23 AM 4/1/2020 updated to include workaround for Test-ModuleManifest failure to reload updated psd1s (uses a force loaded copy of current file), now storing the output of the import-module -force passthrugh ; also added Pester tests for: Author, CompanyName, LicenseURI, PowerShellVersion,CopyRight, !RequiredModule,!ExportedFormatFiles,Prefix,comparre missing Exported functions (to determine which unexported), more detailed checking of Exports, and check CBH for Synopsis,Description & 1+ Example (several are remmed by default)

--- rebuild-module.ps1
+++ rebuild-module.ps1
@@ -41,4 +41,4 @@ PARAM(
     $Verbose = ($VerbosePreference -eq 'Continue') ; 
     write-verbose -verbose:$verbose "`$PSBoundParameters:`n$(($PSBoundParameters|out-string).trim())" ; 
 
-.\process-NewModule.ps1 -ModuleName "verb-dev" -ModDirPath "C:\sc\verb-dev" -Repository "`$localPSRepo" -Merge -showdebug -whatif:$($whatif) ;
+.\process-NewModule.ps1 -ModuleName "verb-dev" -ModDirPath "C:\sc\verb-dev" -Repository "`$localPSRepo" -Merge -RunTest -showdebug -whatif:$($whatif) ;
