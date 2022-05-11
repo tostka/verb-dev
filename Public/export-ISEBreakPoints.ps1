223a1b281e1709a9234b8913d0f51f5ca93aea07
Tostka <tostka@users.noreply.github.com>
2022-05-11 12:13:42 -0500

    Update export-ISEBreakPoints.ps1
     * 10:20 AM 5/11/2022 added whatif support; updated CBH ; expanded echos; cleanedup
        * 8:58 AM 5/9/2022 add: test for bps before exporting

--- Public/export-ISEBreakPoints.ps1
+++ Public/export-ISEBreakPoints.ps1
@@ -15,10 +15,18 @@ function export-ISEBreakPoints {
     Github      : https://github.com/tostka
     Tags        : Powershell,ISE,development,debugging
     REVISIONS
+    * 10:20 AM 5/11/2022 added whatif support; updated CBH ; expanded echos; cleanedup
+    * 8:58 AM 5/9/2022 add: test for bps before exporting
     * 12:56 PM 8/25/2020 fixed typo in 1.0.0 ; init, added to verb-dev module
     .DESCRIPTION
     export-ISEBreakPoints - Export all 'Line' ise breakpoints to XML file
     By default, attempts to save to the same directory as the script, but if the directory specified doesn't exist, it redirects the save to the c:\scripts dir.
+    .PARAMETER PathDefault
+    Default Path for export (when `$Script directory is unavailable)[-PathDefault c:\path-to\]
+    .PARAMETER Script
+    Path to target Script file (defaults to Current ISE Tab fullpath)[-Script c:\path-to\file.ext]
+    .PARAMETER Whatif
+    Parameter to run a Test no-change pass [-Whatif switch]
     .EXAMPLE
     export-ISEBreakPoints
     Export all 'line'-type breakpoints on the current open ISE tab, to a matching xml file
@@ -36,13 +44,17 @@ function export-ISEBreakPoints {
         [string]$PathDefault = 'c:\scripts',
         [Parameter(HelpMessage="(debugging):Path to target Script file (defaults to Current ISE Tab fullpath)[-Script c:\path-to\file.ext]")]
         #[ValidateScript({Test-Path $_})]
-        [string]$Script
+        [string]$Script,
+        [Parameter(HelpMessage="Whatif Flag  [-whatIf]")]
+        [switch] $whatIf
     ) ;
-    BEGIN {} ;
+    BEGIN {
+        ${CmdletName} = $PSCmdlet.MyInvocation.MyCommand.Name ;
+        $verbose = $($VerbosePreference -eq "Continue")} ;
     PROCESS {
         if ($psise){
-            
             if($Script){
+                write-verbose "`$Script:$($Script)" ; 
                 if( ($tScript = (gci $Script).FullName) -AND ($psise.powershelltabs.files.fullpath -contains $tScript)){
                     write-host "-Script specified diverting target to:`n$($Script)" ; 
                     $tScript = $Script ; 
@@ -51,32 +63,36 @@ function export-ISEBreakPoints {
                     throw "-Script specified is not a valid path!`n$($Script)`n(or is not currently open in ISE)" ; 
                 } ; 
             } elseif($psise.CurrentFile.FullPath){
+                write-verbose "(processing `$psise.CurrentFile.FullPath:$($psise.CurrentFile.FullPath)...)"
                 $tScript = $psise.CurrentFile.FullPath ;
                 # default to same loc, variant name of script in currenttab of ise
-                #$xFname=$psise.CurrentFile.FullPath.replace(".ps1","-ps1.xml").replace(".psm1","-psm1.xml").replace(".","-BP.") ;
                 $xFname=$tScript.replace(".ps1","-ps1.xml").replace(".psm1","-psm1.xml").replace(".","-BP.") ;
                 $AllUsrsScripts = "$($env:ProgramFiles)\WindowsPowerShell\Scripts" ; 
                 if( ( (split-path $xFname) -eq $AllUsrsScripts) -OR (-not(test-path (split-path $xFname))) ){
                     # if in the AllUsers profile, or the ISE script dir is invalid
                     if($tdir = get-item "$([Environment]::GetFolderPath('MyDocuments'))\WindowsPowershell\Scripts"){
-                        # if the CUser has a profile Scripts dir, use it                
+                        write-verbose "(`$tDir:CUser has a profile Scripts dir: using it for xml output:`n$($tdir))" ;
                     } elseif($tdir = get-item $PathDefault){
-                        # else if functional use the $pathdefault
+                        write-verbose "(`$tDir:Using `$PathDefault:$($PathDefault))" ; 
                     } else {
                         throw "Unable to resolve a suitable destination for the current script`n$($tScript)" ; 
                         break ; 
                     } ; 
                     $smsg = "broken path, defaulting to: $($tdir.fullname)" ; 
-                    #$xFname=(join-path -path "c:\scripts\" -childpath (split-path $psise.CurrentFile.FullPath -leaf)).replace(".ps1","-ps1.xml").replace(".psm1","-psm1.xml").replace(".","-BP.") ;
                     $xFname = $xFname.replace( (split-path $xFname), $tdir.fullname) ;
                 } ;
             } else { throw "ISE has no current file open. Open a file before using this script" } ; 
         
-        write-host "Creating BP file:$($xFname)" ;
-        #$xBPs= get-psbreakpoint |?{$_.Script -eq $($psise.currentfile.fullpath) -AND ($_.line)} ;
-        $xBPs= get-psbreakpoint |?{ ($_.Script -eq $tScript) -AND ($_.line)} ;
-        $xBPs | Export-Clixml -Path $xFname ;
-        write-host "$(($xBPs|measure).count) Breakpoints exported to $xFname`n$(($xBPs|sort line|ft -a Line,Script|out-string).trim())" ;
+            write-host "Creating BP file:$($xFname)" ;
+            $xBPs= get-psbreakpoint |?{ ($_.Script -eq $tScript) -AND ($_.line)} ;
+            if($xBPs){
+                $xBPs | Export-Clixml -Path $xFname -whatif:$($whatif);
+                $smsg = "$(($xBPs|measure).count) Breakpoints exported to $xFname`n$(($xBPs|sort line|ft -a Line,Script|out-string).trim())" ;
+                if($whatif){$smsg = "-whatif:$($smsg)" };
+                write-host $smsg ; 
+            } else {
+                write-warning "$($tScript): has *no* Breakpoints set!" ; 
+            }
         } else {  write-warning "This script only functions within PS ISE, with a script file open for editing" };
     } # PROC-E
 }
