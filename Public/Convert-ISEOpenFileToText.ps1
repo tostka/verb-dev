702ce350aa5fd5eb575932426f7377ef2c4cf985
Tostka <tostka@users.noreply.github.com>
2026-04-10 17:01:57 -0500

    Update Convert-ISEOpenFileToText.ps1
    * 9:30 AM 4/10/2026 added traytip/msgbox notif of the update

--- Public/Convert-ISEOpenFileToText.ps1
+++ Public/Convert-ISEOpenFileToText.ps1
@@ -19,10 +19,13 @@ Function Convert-ISEOpenFileToText {
     AddedWebsite: https://github.com/jdhitsolutions/ISEScriptingGeek/
     AddedTwitter: URL
     REVISIONS
+    * 9:30 AM 4/10/2026 added traytip/msgbox notif of the update
     * 10:13 AM 4/9/2026 init
     * Jul 3, 2023 jdh posted vers
     .DESCRIPTION
     Convert-ISEOpenFileToText.ps1 - Save current tab file to .txt extension
+    .PARAMETER Reload
+    Switch to reload the original .ps1 file (vs leave .txt copy open)[-Reload]
     .INPUTS
     None. Does not accepted piped input.(.NET types, can add description)
     .OUTPUTS
@@ -35,24 +38,47 @@ Function Convert-ISEOpenFileToText {
     .LINK
     https://github.com/tostka/verb-dev
     .LINK
-    https://github.com/jdhitsolutions/ISEScriptingGeek/    #>
+    https://github.com/jdhitsolutions/ISEScriptingGeek/    
+    #>
     [CmdletBinding()]
     Param (
-        [Switch]$Reload
+        [Parameter(HelpMessage="Switch to reload the original .ps1 file (vs leave .txt copy open)[-Reload]")]
+            [Switch]$Reload
     )
-    #verify we are in the ISE
-    if ($psISE) {        
-        #get the current file name and path and change the extension
-        $PSVersion = $psISE.CurrentFile.FullPath
-        $textVersion = $PSVersion -replace '.ps1', '-ps1.txt'
-        #save the file.
-        $psISE.CurrentFile.SaveAs($textVersion)
-        #if -Reload then reload the PowerShell file into the ISE
-        if ($Reload) {
-            $psISE.CurrentPowerShellTab.Files.Add($PSVersion)
+    BEGIN{
+        $TipTimeSecs = 5        
+    }PROCESS{
+        if ($psISE) {                    
+            #get the current file name and path and change the extension
+            $PSVersion = $psISE.CurrentFile.FullPath
+            $textVersion = $PSVersion -replace '.ps1', '-ps1.txt'
+            #save the file.
+            $psISE.CurrentFile.SaveAs($textVersion)
+            # traytip the update status
+            $tipTitle = "ISE update";
+            $tipText =  "ISE: current tab saved as .txt";
+            if(gcm show-traytip -ea 0){
+                 show-traytip -Type info -Text $tipText -Title $tipTitle  -ShowTime $TipTimeSecs ; 
+            }elseif(gcm show-MsgBox -ea 0){
+                 show-traytip -Type info -Text $tipText -Title $tipTitle  -ShowTime $TipTimeSecs ; 
+                 Show-MsgBox -Prompt $tipText -Title $tipTitle -Icon Information -BoxType OkOnly
+            }else{
+                $shell = New-Object -ComObject WScript.Shell
+                $shell.Popup($tipText, 5,  $tipTitle , 64) ; 
+            }
+            #if -Reload then reload the PowerShell file into the ISE
+            if ($Reload) {
+                $psISE.CurrentPowerShellTab.Files.Add($PSVersion)
+            }
+        } else {
+            Write-Warning 'This function requires the Windows PowerShell ISE.'
         }
-    } #if $psISE
-    else {
-        Write-Warning 'This function requires the Windows PowerShell ISE.'
+    }
+    END{
+        if(gcm show-traytip -ea 0){
+            # Recommended: Clean up the icon after use to avoid it lingering in the tray
+            Wait-Event -Timeout 5 # Optional delay
+            TRY{$notification.Dispose()}CATCH{} # eat error, after tray closes
+        } ; 
     }
 } #end function
